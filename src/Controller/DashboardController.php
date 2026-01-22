e7a32d8b7684181c7317f01a3561c1c2c436f77f
Noa Fraigneau <nfra@pronic.local>
2026-01-22 14:44:14 +0100

    3rd commit

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,18 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
-    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        $user = $this->getUser();
-        
         $dashboardData = [
-            'userName' => $user->getUserIdentifier(),
             'currentDate' => new \DateTime(),
         ];
 
